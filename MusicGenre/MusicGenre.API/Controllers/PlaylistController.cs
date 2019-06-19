df10e0de82032c78bf2a1976c931fca347870283
Alef Hortelã <alef.hortela@gmail.com>
2019-06-19 16:00:44 -0300

    Created controller and configurated startup

---
+++ MusicGenre/MusicGenre.API/Controllers/PlaylistController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicGenre.Application.Interface;
+using MusicGenre.Application.ViewModel;
+
+namespace MusicGenre.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistController : ControllerBase
+    {
+        private readonly IWeatherApp weatherApp;
+
+        public PlaylistController(IWeatherApp _weatherApp)
+        {
+            weatherApp = _weatherApp;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="city"></param>
+        /// <returns></returns>
+        [HttpGet("{city}")]
+        public ResultAPIViewModel GetPlaylistByCity(string city)
+        {
+            return weatherApp.getTracksByCityName(city);
+        }
+    }
+}
--- MusicGenre/MusicGenre.API/Startup.cs
+++ MusicGenre/MusicGenre.API/Startup.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using MusicGenre.API.Configurations;
+using MusicGenre.Application.AutoMapper;
 
 namespace MusicGenre.API
 {
@@ -15,6 +19,16 @@ namespace MusicGenre.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var mappingConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new DomainToViewModelProfile());
+            });
+
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+            services.AddDIConfiguration();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +38,20 @@ namespace MusicGenre.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseHsts();
+            }
+
+            app.UseCors(c =>
+            {
+                c.AllowAnyHeader();
+                c.AllowAnyMethod();
+                c.AllowAnyOrigin();
             });
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
         }
     }
 }
