d8e7e4a8242cfdac64f84905ccc3fc121f23b4aa
Alef Hortela <alef.hortela@gmail.com>
2019-06-18 23:32:25 -0300

    Created viewmodels to Deezer Track an Artist

--- MusicGenre/MusicGenre.Application/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MusicGenre.Application
-{
-    public class Class1
-    {
-    }
-}
---
+++ MusicGenre/MusicGenre.Application/ViewModel/DeezerTrackViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MusicGenre.Application.ViewModel
+{
+    public class DeezerTrackViewModel
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public DeezerArtistViewModel Artist { get; set; }
+    }
+}
